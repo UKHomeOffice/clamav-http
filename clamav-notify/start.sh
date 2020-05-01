8651d3abc621e1623d875c44a35c92186ff033f6
Adam Smith <adam.smith@appvia.io>
2020-05-01 12:13:49 +0100

    notify deployment tweaks

--- clamav-notify/start.sh
+++ clamav-notify/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-touch /var/lib/clamav/
-
+LIGHTTPD_ROOT="${LIGHTTPD_ROOT:-/var/lib/clamav}"
 LIGHTTPD_CONFIG="${LIGHTTPD_CONFIG:-lighttpd.conf}"
+
+mkdir -p $LIGHTTPD_ROOT
+touch "$LIGHTTPD_ROOT/index.htm"
 
 freshclam -d
 lighttpd -t -f $LIGHTTPD_CONFIG
