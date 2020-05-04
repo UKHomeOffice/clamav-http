7c4ffcb9a1641c05c062c3348a4c3e498e29f3e0
Adam Smith <adam.smith@appvia.io>
2020-05-04 11:14:47 +0100

    Rejig

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 
 docker login -u="ukhomeofficedigital+acp_clamav" -p=${DOCKER_PASSWORD} quay.io
 
-for image in 'clamav' 'clamav-http' 'clamav-notify'
+for image in 'clamav' 'clamav-http' 'clamav-notify' 'clamav-notify-cron'
 do
   docker build --no-cache -t "quay.io/ukhomeofficedigital/acp-$image:$DRONE_COMMIT_SHA" "$image"
   for tag in "$@"
--- clamav-notify/start.sh
+++ clamav-notify/start.sh
@@ -5,7 +5,6 @@ LIGHTTPD_ROOT="${LIGHTTPD_ROOT:-/var/lib/clamav}"
 LIGHTTPD_CONFIG="${LIGHTTPD_CONFIG:-lighttpd.conf}"
 
 mkdir -p $LIGHTTPD_ROOT
-touch "$FRESHCLAM_ROOT/index.htm"
 touch "$LIGHTTPD_ROOT/index.htm"
 
 freshclam -d
