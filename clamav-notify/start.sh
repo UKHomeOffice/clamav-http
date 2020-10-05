1ea0f0a66e62c19e881e2cee8d72ebe6a45681b6
Adam Smith <adam.smith@appvia.io>
2020-10-05 11:06:08 +0100

    start.sh debug

--- clamav-notify/start.sh
+++ clamav-notify/start.sh
@@ -3,8 +3,18 @@
 FRESHCLAM_ROOT="${FRESHCLAM_ROOT:-/var/lib/clamav}"
 FRESHCLAM_CONFIG="${FRESHCLAM_CONFIG:-/etc/clamav/freshclam.conf}"
 
+echo "Freshclam root: $FRESHCLAM_ROOT"
+echo "Freshclam config: $FRESHCLAM_CONFIG"
+
+cat $FRESHCLAM_CONFIG
+
 LIGHTTPD_ROOT="${LIGHTTPD_ROOT:-/var/lib/clamav}"
 LIGHTTPD_CONFIG="${LIGHTTPD_CONFIG:-lighttpd.conf}"
+
+echo "Lighttpd root: $LIGHTTPD_ROOT"
+echo "Lighttpd config: $LIGHTTPD_CONFIG"
+
+cat $LIGHTTPD_CONFIG
 
 mkdir -p $LIGHTTPD_ROOT
 touch "$LIGHTTPD_ROOT/index.htm"
