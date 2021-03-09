78980f300eebf7b23976d87994f5063de4ccd1c1
tom.england <tom.england@digital.homeoffice.gov.uk>
2021-03-09 17:09:57 +0000

    Updated clamav mirror to serve signatures on first deployment

--- clamav-notify/start.sh
+++ clamav-notify/start.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
+# 
+# Starts AV mirror and seeds with latest definitions if they dont already exist
+
+set -o errexit
 
 FRESHCLAM_ROOT="${FRESHCLAM_ROOT:-/var/lib/clamav}"
 FRESHCLAM_CONFIG="${FRESHCLAM_CONFIG:-/etc/clamav/freshclam.conf}"
-
-echo "Freshclam root: $FRESHCLAM_ROOT"
-echo "Freshclam config: $FRESHCLAM_CONFIG"
-
-cat $FRESHCLAM_CONFIG
-
 LIGHTTPD_ROOT="${LIGHTTPD_ROOT:-/var/lib/clamav/mirror}"
 LIGHTTPD_CONFIG="${LIGHTTPD_CONFIG:-lighttpd.conf}"
 
-echo "Lighttpd root: $LIGHTTPD_ROOT"
-echo "Lighttpd config: $LIGHTTPD_CONFIG"
 
-cat $LIGHTTPD_CONFIG
+function err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
 
-mkdir -p $LIGHTTPD_ROOT
-touch "$LIGHTTPD_ROOT/index.htm"
 
-freshclam -d --config-file=$FRESHCLAM_CONFIG
+function log() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@"
+}
 
-lighttpd -t -f $LIGHTTPD_CONFIG
-lighttpd -D -f $LIGHTTPD_CONFIG
+
+function showconfig() {
+  log "Freshclam root: $FRESHCLAM_ROOT"
+  log "Freshclam config: $FRESHCLAM_CONFIG"
+  log "Lighttpd root: $LIGHTTPD_ROOT"
+  log "Lighttpd config: $LIGHTTPD_CONFIG"
+  cat $LIGHTTPD_CONFIG
+  cat $FRESHCLAM_CONFIG
+}
+
+
+##############################################
+# Create directories to host mirror, copy over
+# defs if applicable and start daemon.
+# Globals:
+#   LIGHTHTTPD_ROOT
+#   FRESHCLAM_ROOT
+#   FRESHCLAM_CONFIG 
+#   FRESHCLAM_ROOT
+# Arguments:
+#   None
+# Outputs:
+#   Writes log to stdout
+##############################################
+function startmirror() {
+  mkdir -p $LIGHTTPD_ROOT
+  touch "$LIGHTTPD_ROOT/index.htm"
+  # Run freshclam in foreground once to get latest defs on container start
+  freshclam --config-file=$FRESHCLAM_CONFIG --stdout
+  # If no defs exist in mirror, copy them
+  yes n | cp -i $FRESHCLAM_ROOT/*.cvd $LIGHTTPD_ROOT 2>/dev/null
+  # Set freshclam to run in background
+  freshclam -d --config-file=$FRESHCLAM_CONFIG
+  # Start http server
+  lighttpd -t -f $LIGHTTPD_CONFIG
+  lighttpd -D -f $LIGHTTPD_CONFIG
+}
+
+
+function main() {
+  showconfig
+  startmirror
+}
+
+main "@"
--- example/README.md
+++ example/README.md
@@ -7,8 +7,13 @@ Minikube by default does not include the CRD's for cert-manager.
 
 Add certmanager to minikube
 
+`kubectl create namespace cert-manager`
+
+`helm repo add jetstack https://charts.jetstack.io && helm repo update`
+
 ```
-helm install \  cert-manager jetstack/cert-manager \                 
+helm install \
+  cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --version v1.2.0 \
   --create-namespace \
